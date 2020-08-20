db7a5a0fc96bf643be8ebb940d6c52f677841b48
a.fokin <a.fokin@sportradar.com>
2020-08-20 14:41:50 +0200

    Change MoveAndCollide to MoveAndSlide

--- player/Player.cs
+++ player/Player.cs
@@ -4,11 +4,11 @@ using System;
 public class Player : KinematicBody2D
 {
     private int speed = 250;
-    
+
     public override void _PhysicsProcess(float delta) {
         var velocity = new Vector2();
         velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");    
         velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-        MoveAndCollide(velocity.Normalized() * speed * delta);
+        MoveAndSlide(velocity.Normalized() * speed);
     }
 }
