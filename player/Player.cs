47162d3ea389a8e8854a996d22bcae3e59ef486f
a.fokin <a.fokin@sportradar.com>
2020-08-21 16:09:50 +0200

    Finish idle and walk animations

--- player/Player.cs
+++ player/Player.cs
@@ -5,25 +5,32 @@ public class Player : KinematicBody2D
 {
     private int speed = 100;
     private AnimationPlayer animationPlayer;
+    private AnimationTree animationTree;
+    private AnimationNodeStateMachinePlayback animationState;
 
     public override void _Ready(){
         animationPlayer = GetNode<AnimationPlayer>("Animation/AnimationPlayer");
+        animationTree = GetNode<AnimationTree>("Animation/AnimationTree");
+        animationState = animationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
     }
 
     public override void _PhysicsProcess(float delta) {
         var velocity = Vector2.Zero;
         velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");    
         velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+        velocity = velocity.Normalized();
 
-        if (velocity == Vector2.Zero)
+        if (velocity != Vector2.Zero)
         {
-            animationPlayer.Play("idle_right");
+            animationTree.Set("parameters/idle/blend_position", velocity);
+            animationTree.Set("parameters/walk/blend_position", velocity);
+            animationState.Travel("walk");
         } 
         else 
         {
-            animationPlayer.Play("walk_right");
+          animationState.Travel("idle");
         }
 
-        MoveAndSlide(velocity.Normalized() * speed);
+        MoveAndSlide(velocity * speed);
     }
 }
