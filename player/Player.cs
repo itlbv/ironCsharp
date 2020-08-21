407c1d55d7b57aebc03bd187b8085b2b11dd5d90
a.fokin <a.fokin@sportradar.com>
2020-08-21 14:07:54 +0200

    Add animation nodes, add idle_right and walk_right animations

--- player/Player.cs
+++ player/Player.cs
@@ -3,12 +3,27 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    private int speed = 250;
+    private int speed = 100;
+    private AnimationPlayer animationPlayer;
+
+    public override void _Ready(){
+        animationPlayer = GetNode<AnimationPlayer>("Animation/AnimationPlayer");
+    }
 
     public override void _PhysicsProcess(float delta) {
-        var velocity = new Vector2();
+        var velocity = Vector2.Zero;
         velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");    
         velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+
+        if (velocity == Vector2.Zero)
+        {
+            animationPlayer.Play("idle_right");
+        } 
+        else 
+        {
+            animationPlayer.Play("walk_right");
+        }
+
         MoveAndSlide(velocity.Normalized() * speed);
     }
 }
