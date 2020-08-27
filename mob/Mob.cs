da2a7f8a53add3f5046fb519127ea25a5c995947
a.fokin <a.fokin@sportradar.com>
2020-08-27 14:50:13 +0200

    Add blank Actions node to Mob

--- mob/Mob.cs
+++ mob/Mob.cs
@@ -2,6 +2,13 @@ using Godot;
 
 public class Mob : KinematicBody2D
 {
+    Actions Actions;
+
+    public override void _Ready()
+    {
+        Actions = GetNode<Actions>("Actions");
+    }
+
     public void MoveToMob(Mob targetMob)
     {
         GD.Print("move to mob");
---
+++ mob/actions/Actions.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class Actions : Node2D
+{
+    
+    public override void _Ready()
+    {
+        GD.Print("Actions created");
+    }
+}
